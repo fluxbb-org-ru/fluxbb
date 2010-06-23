09c38480b8106df7e9d48352d0ced4fbddea2b6e
ruflux <ruflux@example.com>
2010-06-23 19:04:56 +0000

    Branch 1.4.0 with mods

---
+++ branches/1.4.0-with-mods/lang/Russian/admin_ranks.php
@@ -0,0 +1,21 @@
+<?php
+
+// Language definitions used in admin_ranks.php
+$lang_admin_ranks = array(
+
+'Must be integer message'	=>	'Minimum posts must be a positive integer value.',
+'Dupe min posts message'	=>	'There is already a rank with a minimun posts value of %s.',
+'Must enter title message'	=>	'You must enter a rank title.',
+'Rank added redirect'		=>	'Rank added. Redirecting …',
+'Rank updated redirect'		=>	'Rank updated. Redirecting …',
+'Rank removed redirect'		=>	'Rank removed. Redirecting …',
+'Ranks head'				=>	'Ранги',
+'Add rank subhead'			=>	'Добавить ранг',
+'Add rank info'				=>	'Введите ранг и минимальное количество сообщений, которое должен ввести пользователь, чтобы получить этот ранг. Разные ранги не должны иметь одинаковое количество сообщений. Если у пользователя задан титул, он будет показан вместо ранга. <strong>Пользовательские ранги должны быть включены в %s, иначе они не будут действовать</strong>.',
+'Rank title label'			=>	'Ранг',
+'Minimum posts label'		=>	'Минимум сообщений',
+'Actions label'				=>	'Действие',
+'Edit remove subhead'		=>	'Изменение/удаление рангов',
+'No ranks in list'			=>	'Список рангов пуст',
+
+);
---
+++ branches/1.4.0-with-mods/lang/languages.php
@@ -0,0 +1,8 @@
+<?php
+
+// Language definitions for quick selection
+$languages = array(
+
+'Рус'	=>	'Russian',
+'Eng'	=>	'English'
+);
