df4ed2c772baca760deb960a8585ac291dad20e6
ruflux <ruflux@example.com>
2010-10-09 15:07:52 +0000

    Error in edit breadcrumb

--- branches/pe/edit.php
+++ branches/pe/edit.php
@@ -130,7 +130,7 @@ $cur_index = 1;
 <div class="linkst">
 	<div class="inbox">
 		<ul class="crumbs">
-			<li><a href="index.php<?php echo $kind ?>"><?php echo $lang_common['Boards kind'][$kind] ?></a></li>
+			<li><a href="index.php?kind=<?php echo $kind ?>"><?php echo $lang_common['Boards kind'][$kind] ?></a></li>
 			<li><span>»&#160;</span><a href="viewforum.php?id=<?php echo $cur_post['fid'] ?>"><?php echo pun_htmlspecialchars($cur_post['forum_name']) ?></a></li>
 			<li><span>»&#160;</span><a href="viewtopic.php?id=<?php echo $cur_post['tid'] ?>"><?php echo pun_htmlspecialchars($cur_post['subject']) ?></a></li>
 			<li><span>»&#160;</span><strong><?php echo $lang_post['Edit post'] ?></strong></li>
